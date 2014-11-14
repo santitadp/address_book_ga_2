d410d6fd68b5a7368afbc3fa513bd353debe7b8c
Santita Dwi Putri <santitadwiputri@Santitas-MacBook-Pro.local>
2014-11-13 23:26:01 -0500

    Created phone number loop in add_entry.rb

--- models/add_entry.rb
+++ models/add_entry.rb
@@ -10,47 +10,59 @@ def add_entry
 	puts "Last name: "
 	entry.last_name = gets.chomp
 
-	phone = PhoneNumber.new
+	loop do
 
-	puts "Phone number (no dashes): "
-	phone.phone_number = gets.chomp
+	puts "Would you like to enter a phone number? (Y/N)"
+	response = gets.chomp
 
-	puts "Phone number category: "
-	phone.category = gets.chomp
+		if response == "Y" or response == "y"
 
-	# puts "Would you like to enter additional phone numbers? (Y/N)"
-	# 	response = gets.chomp
-	# 	while response == "Y"
-	# 			puts "You're entering additional phone numbers."
-	# 		if response == "N"
-	# 			puts "You're not entering additional phone numbers."
-	# 		else
-	# 			puts "Please enter Y/N"
-	# 		end
-	# 	end
+			phone = PhoneNumber.new
 
-	email = Email.new
+			puts "Phone number: "
+			phone.phone_number = gets.chomp
 
-	puts "Email address: "
-	email.email = gets.chomp
+			puts "Phone number category: "
+			phone.category = gets.chomp
 
-	puts "Email category: "
-	email.category = gets.chomp
+			entry.phone_numbers.push(phone)
 
-	# puts "Would you like to enter additional email addresses? (Y/N)"
-	# 	response = gets.chomp
-	# 	while response == "Y"
-	# 			puts "Yes"
-	# 		if response == "N"
-	# 			puts "No!"
-	# 		else 
-	# 			puts "Please enter Y/N"	
-	# 		end
-	# 	end
+		elsif response == "N" or response == "n"
+			break
+		else
+			puts "Please enter Y/N"
+		end
+	end
+
+	puts "Would you like to enter an e-mail address? (Y/N)"
+	response = gets.chomp
+
+	loop do
+
+		if response == "Y" or "y"
+
+			email = Email.new
+
+			puts "Email address: "
+			email.email = gets.chomp
+
+			puts "Email category: "
+			email.category = gets.chomp
+
+			entry.emails.push(email)
+
+			puts "Would you like to enter additional e-mail addresses? (Y/N)"
+			response = gets.chomp
+
+		elsif response == "N" or "n"
+			break
+		else
+			puts "Please enter Y/N"
+		end
+	end
+
 	entry.save
 	phone.save
 	email.save
-
-	puts AddressEntry.first.inspect
 
 end
